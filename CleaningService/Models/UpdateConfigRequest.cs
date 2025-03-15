1281bf0aa6d7be1e643e8f41fd438590eb9c7b7f
Evgeniy <120410817+nachos503@users.noreply.github.com>
2025-03-15 06:46:14 +0300

    Update UpdateConfigRequest.cs

--- CleaningService/Models/UpdateConfigRequest.cs
+++ CleaningService/Models/UpdateConfigRequest.cs
@@ -3,7 +3,6 @@
     public class UpdateConfigRequest
     {
         public int ConflictRetryCount { get; set; }
-        public double MovementSpeed { get; set; }
         public int NumberOfCleaningVehicles { get; set; }
     }
 }
